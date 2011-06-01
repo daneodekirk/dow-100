fd39265794d3f12c4943cfd1ab30cdfaeb9b071f
daneodekirk <daneodekirk@b8237495-a56b-90ef-ff0d-10ceb266eae1>
2011-06-01 02:13:33 +0000

    Fixed bug in 3-4 smoothing

--- trunk/resources/mean.py
+++ trunk/resources/mean.py
@@ -21,7 +21,7 @@ class Mean:
         info = self.cache[i:i+4]
         time = self.cache[i][0]
 
-        points = [item[0] for item in info]
+        points = [item[1] for item in info]
         added = sum(points)/4
 
         self.output.append([added, time])
@@ -31,7 +31,6 @@ class Mean:
     def mean(self):
         data = [ self.threes(i, 3) for i in range(len(self.data)) 
                         if 0 < i < len(self.data)-1 ] 
-        return data
-        #[TODO] Four smoothing causing an error
-        #newdata = [ self.fours(i, 3) for i in range(len(data)) 
-                        #if 0 < i < len(data)-1 ] 
+        newdata = [ self.fours(i, 3) for i in range(len(data)) 
+                        if 0 < i < len(data)-1 ] 
+        return newdata
