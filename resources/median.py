42b4fdd95d75d6f0525e3d7665c011f6edd0ea67
daneodekirk <daneodekirk@b8237495-a56b-90ef-ff0d-10ceb266eae1>
2011-06-02 03:00:03 +0000

    Refined running median math. Drastically shortening the code.

--- resources/median.py
+++ resources/median.py
@@ -1,40 +1,35 @@
 import math
+import logging
 
 class Median:
     def __init__(self, data, s=3):
         self.data = data 
         self.chunk = s
-    
-    def average(self, i):
-        s = int(math.floor(self.chunk/2))
-        s2 = s+1
-
-        info = self.data[i-s2:i+s]
-        time = self.data[i][0]
-
-        del self.data[i-s2:i+s]
-
-        points = [float(item[1]) for item in info]
-        added = sorted(points)
-        if len(added) > 0:
-            logging.debug(added[-1])
-            added = added[-1]
-
-        #return [time, added/self.chunk]
-        return [time, added]
-
 
     def endpoint(self, i):
+        """ Caculates the endpoints of the running median 
+            based on the assumption of using chunks of 3
+        """
+        
         n = 1 if i == 0 else -1
         #note: weighted value is already a median
         f1, g1, g2 = self.data[i][0], self.data[i+n][0], self.data[i+(2*n)][0]
         return sorted([f1, g1, (3 * g1 - 2 * g2)])[1]
 
     def median(self):
-        medians = [ self.average(i) for i in range(len(self.data)) 
-                        if 0 < i < len(self.data)-1 ] 
+        """ This runs through the data set and orders by medians.
+            Currently uses sets of three.
+        """
+        
+        #remove the time domain
+        d = [item[1] for item in self.data]
 
-        #medians[0][0] = self.endpoint(0)
-        #medians[-1][0] = self.endpoint(len(medians) - 1)
-        return medians
+        #resort as medians
+        medians = [ sorted(d[i-1:i+2])[1] for i in range(len(d))
+                        if 0 < i < len(d)-1 ] 
+        #put time domain back in
+        points = [ [self.data[i+1][0], medians[i] ] 
+                        for i in range(len(medians)) ]
+
+        return points
 
